9a9102ab2462843ebac9b0ac12fc33a61744d2ef
Roberta Neukamp Guerreiro <betaneuk@hotmail.com>
2022-11-09 19:04:10 -0300

    Aula 2 e 3

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
+import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
-  const estilosDaHomePage = { backgroundColor: "red" };
+  const [valorDoFiltro, setValorDoFiltro] = React.useState("");
 
   return (
     <>
-        <CSSReset />
         <div style={{
                 display: "flex",
                 flexDirection: "column",
                 flex: 1,
                 // backgroundColor: "red",
             }}>
-            <Menu />
+              {/* Prop Drilling */}
+            <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
             <Header />
-            <Timeline playlists={config.playlists}>
+            <Timeline searchValue={valorDoFiltro} playlists={config.playlists} >
                 Conteúdo
             </Timeline> 
         </div>
@@ -33,13 +33,14 @@ export default HomePage;
 // }
 
 const StyledHeader = styled.div`
+  background-color: ${({ theme }) => theme.backgroundLevel1};
+
   img {
     width: 80px;
     height: 80px;
     border-radius: 50%;
   }
   .user-info {
-    margin-top: 50px;
     display: flex;
     align-items: center;
     width: 100%;
@@ -47,11 +48,18 @@ const StyledHeader = styled.div`
     gap: 16px;
   }
 `;
+
+const StyledBanner = styled.div`
+  background-color: blue;
+  background-image: url(${({ bg }) => bg });
+  /* background-image: url(${config.bg}); */
+  height: 230px;
+` ;
+
 function Header() {
   return (
     <StyledHeader>
-      {/* <img src="banner" /> */}
-
+      <StyledBanner bg={config.bg} />
       <section className="user-info">
         <img src={`https://github.com/${config.github}.png`} />
         <div>
@@ -63,7 +71,7 @@ function Header() {
   );
 }
 
-function Timeline(propriedades) {
+function Timeline({ searchValue, ...propriedades }) {
   const playlistNames = Object.keys(propriedades.playlists);
   //Statement
   //Retorno por expressão
@@ -72,16 +80,22 @@ function Timeline(propriedades) {
       {playlistNames.map((playlistName) => {
         const videos = propriedades.playlists[playlistName];
         return (
-          <section>
+          <section key={playlistName}>
             <h2>{playlistName}</h2>
             <div>
-              {videos.map((video) => {
-                return (
-                  <a href={video.url}>
-                    <img src={video.thumb} />
-                    <span>{video.title}</span>
-                  </a>
-                );
+              {videos
+                .filter((video) => {
+                  const titleNormalized = video.title.toLowerCase();
+                  const searchValueNormalized = searchValue.toLowerCase();
+                  return titleNormalized.includes(searchValueNormalized)
+                })
+                .map((video) => {
+                  return (
+                    <a key={video.url} href={video.url}>
+                      <img src={video.thumb} />
+                      <span>{video.title}</span>
+                    </a>
+                  );
               })}
             </div>
           </section>
